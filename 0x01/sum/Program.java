1582c8d83cc90086c3a17b9706c1e5a51eddfb56
mphomosia194 <mphomosia194@gmail.com>
2026-02-11 10:44:49 +0000

    Task 3: Sum

---
+++ 0x01/sum/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", TheNumber.DoSum(24, 32));
+        System.out.printf("%d\n", TheNumber.DoSum(5, 0));
+        System.out.printf("%d\n", TheNumber.DoSum(4, -2));
+    }
+}
---
+++ 0x01/sum/TheNumber.java
@@ -0,0 +1,5 @@
+public class TheNumber {
+    public static int DoSum(int a, int b) {
+        return a + b;
+    }
+}
