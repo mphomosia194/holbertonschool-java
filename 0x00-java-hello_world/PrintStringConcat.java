d31a6c2796eed7bed5a37aba4ccde0fd1165f212
mphomosia194 <mphomosia194@gmail.com>
2026-02-11 09:54:35 +0000

    Task 4: Concatenation

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton";
+        String text2 = "School";
+        String sentence = "Welcome to " + text1 + " " + text2 + " !";
+        System.out.println(sentence);
+    }
+}
