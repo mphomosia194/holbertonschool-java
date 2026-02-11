b06b5ca0470276a08d699ab54cf201e14b336331
mphomosia194 <mphomosia194@gmail.com>
2026-02-11 10:20:43 +0000

    Task 1: Even numbers from 0 to 99

---
+++ 0x01/even/EvenNumbers.java
@@ -0,0 +1,12 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < 100; i += 2) {
+            if (i != 98)
+                output.append(i).append(", ");
+            else
+                output.append(i);
+        }
+        System.out.println(output);
+    }
+}
