c0f597e8a9c155c4fa956fc668fe39f5f8731245
mphomosia194 <mphomosia194@gmail.com>
2026-02-11 09:52:36 +0000

    Task 3: Print String Variable

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String text = "Holberton School";
+        System.out.println(text + "\n" + text + "\n" + text.substring(0, 9));
+    }
+}
