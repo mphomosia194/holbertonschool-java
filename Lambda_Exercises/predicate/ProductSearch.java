ad0162f6306d107c96ad3650d82e5966d0c2aa14
mphomosia194 <mphomosia194@gmail.com>
2026-03-24 13:44:30 +0000

    Fix ProductSearch to use Predicate with raw List for checker

--- Lambda_Exercises/predicate/ProductSearch.java
+++ Lambda_Exercises/predicate/ProductSearch.java
@@ -2,10 +2,11 @@ import java.util.*;
 import java.util.function.Predicate;
 
 public class ProductSearch {
-    public static List<Product> filter(List<Product> products, Predicate<Product> predicate) {
-        List<Product> result = new ArrayList<>();
+    public static List filter(List products, Predicate<Product> predicate) {
+        List result = new ArrayList();
 
-        for (Product p : products) {
+        for (Object obj : products) {
+            Product p = (Product) obj;
             if (predicate.test(p)) {
                 result.add(p);
             }
