b782b99a69e80a17c106dbf1e491be2587cb94ab
mphomosia194 <mphomosia194@gmail.com>
2026-03-27 13:28:08 +0000

    Final fix: Predicate<Product> with generics and grep match

--- Lambda_Exercises/predicate/ProductSearch.java
+++ Lambda_Exercises/predicate/ProductSearch.java
@@ -1,16 +1,13 @@
 import java.util.*;
 import java.util.function.Predicate;
-
 public class ProductSearch {
 public static List<Product> filter(List<Product> products, Predicate<Product> predicate) {
 List<Product> result = new ArrayList<>();
-
 for (Product p : products) {
 if (predicate.test(p)) {
 result.add(p);
 }
 }
-
 return result;
 }
 }
