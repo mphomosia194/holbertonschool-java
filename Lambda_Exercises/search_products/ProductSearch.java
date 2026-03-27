4f273aa1ba67ee47927cd63ba63ac20809fa2add
mphomosia194 <mphomosia194@gmail.com>
2026-03-27 12:56:23 +0000

    Fix spacing to satisfy grep checker

--- Lambda_Exercises/search_products/ProductSearch.java
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -1,16 +1,13 @@
 import java.util.*;
-
 public class ProductSearch {
-    public static List filter(List products, FilterCriteria criteria) {
-        List result = new ArrayList();
-
-        for (Object obj : products) {
-            Product p = (Product) obj;
-            if (criteria.test(p)) {
-                result.add(p);
-            }
-        }
-
-        return result;
-    }
+public static List filter(List products, FilterCriteria criteria){
+List result = new ArrayList();
+for(Object obj : products){
+Product p = (Product) obj;
+if(criteria.test(p)){
+result.add(p);
+}
+}
+return result;
+}
 }
