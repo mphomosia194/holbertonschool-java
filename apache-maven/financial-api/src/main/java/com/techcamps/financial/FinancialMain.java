34818eb9fc225adf299bfae9b573b739a0a5caad
mphomosia194 <mphomosia194@gmail.com>
2026-03-30 10:36:17 +0000

    Fix Java version compatibility in Maven compiler settings

---
+++ apache-maven/financial-api/src/main/java/com/techcamps/financial/FinancialMain.java
@@ -0,0 +1,7 @@
+package com.techcamps.financial;
+
+public class FinancialMain {
+    public static void main(String[] args) {
+        System.out.println("Creating a project Java Maven.");
+    }
+}
