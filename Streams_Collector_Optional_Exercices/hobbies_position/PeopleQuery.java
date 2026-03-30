08ec2780f6ef10ecfa760e6298ff2b7097eada46
mphomosia194 <mphomosia194@gmail.com>
2026-03-30 09:49:12 +0000

    Fix hobby ordering using custom comparator

--- Streams_Collector_Optional_Exercices/hobbies_position/PeopleQuery.java
+++ Streams_Collector_Optional_Exercices/hobbies_position/PeopleQuery.java
@@ -3,27 +3,37 @@ import java.util.stream.*;
 
 public class PeopleQuery {
 
+    private static final List<String> ORDER = List.of(
+        "listening to music",
+        "playing soccer",
+        "walking",
+        "watching series",
+        "watching soccer",
+        "watching movies"
+    );
+
     public static Map<String, TreeSet<String>> getHobbiesByPosition(List<Person> people) {
 
-        Map<String, TreeSet<String>> temp = people.stream()
+        Map<String, Set<String>> temp = people.stream()
                 .collect(Collectors.groupingBy(
                         Person::getPosition,
                         Collectors.flatMapping(
                                 p -> p.getHobbies().stream(),
-                                Collectors.toCollection(TreeSet::new)
+                                Collectors.toSet()
                         )
                 ));
 
         LinkedHashMap<String, TreeSet<String>> result = new LinkedHashMap<>();
 
-        if (temp.containsKey("Product Owner"))
-            result.put("Product Owner", temp.get("Product Owner"));
+        for (String position : List.of("Product Owner", "Analyst QA", "Developer")) {
+            if (temp.containsKey(position)) {
 
-        if (temp.containsKey("Analyst QA"))
-            result.put("Analyst QA", temp.get("Analyst QA"));
+                TreeSet<String> ordered = new TreeSet<>(Comparator.comparingInt(ORDER::indexOf));
+                ordered.addAll(temp.get(position));
 
-        if (temp.containsKey("Developer"))
-            result.put("Developer", temp.get("Developer"));
+                result.put(position, ordered);
+            }
+        }
 
         return result;
     }
