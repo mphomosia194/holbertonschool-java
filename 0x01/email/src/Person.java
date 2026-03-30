1a18e61a8af2aa7ae2ddb1dbae14f776f129b204
mphomosia194 <mphomosia194@gmail.com>
2026-03-30 12:35:24 +0000

    Fix JUnit setup and ensure tests compile and run

---
+++ 0x01/email/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+
+    public static boolean isEmailValid(String email) {
+        if (email == null) return false;
+        if (!email.contains("@")) return false;
+        if (email.length() > 50) return false;
+        return true;
+    }
+}
